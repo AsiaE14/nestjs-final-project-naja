4eb343f357e8cf4ce6d91bc14610536aa74c3cfb
Mei-Yoko <keypongphob@gmail.com>
2026-02-23 21:05:36 +0700

    utils/json store

--- src/common/utils/json-storage.util.ts
+++ src/common/utils/json-storage.util.ts
@@ -39,4 +39,49 @@ export class JsonStroage<T extends {id: String} >{
     async findAll (): Promise<T[]> {
         return [...this.data];
     }
+
+    async findById(Id: string): Promise<T | undefined> {
+        return this.data.find(item => item.id === Id);
+    }
+
+    async create(item: T): Promise<T> {
+        this.data.push(item);
+        await this.save();
+        return item;
+    }
+
+    async update(id: string, updates: Partial<T>): Promise<T | undefined> {
+        const index = this.data.findIndex(item => item.id === id);
+        if (index === -1) {
+          return undefined;
+        }
+        this.data[index] = { ...this.data[index], ...updates };
+        await this.save();
+        return this.data[index];
+      }
+
+    async delete(id: string): Promise<boolean>{
+        const index = this.data.findIndex(item => item.id === id);
+        if (index === -1){
+            return false;
+        }
+        this.data.splice(index, 1);
+        await this.save();
+        return true;
+    }
+
+    async findByCondition(predicate: (item: T) => boolean): Promise<T[]> {
+        return this.data.filter(predicate);
+    }
+
+    async deleteByConditon(predicate: (item: T) => boolean): Promise<number>{
+        const initialLength = this.data.length;
+        this.data = this.data.filter(item => !predicate(item));
+        const deleteCount = initialLength - this.data.length;
+
+        if(deleteCount > 0){
+            await this.save
+        }
+        return deleteCount;
+    }
 }
